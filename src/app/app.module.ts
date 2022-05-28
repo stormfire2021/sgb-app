387062e79babe942b74d1f26e9ee343f247e53f2
Fabiula Pellicciotti <fabiula.pellicciotti@gmail.com>
2022-05-28 18:32:18 -0300

    Update develop 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
-import { CentralComponent } from './central/central.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     FooterComponent,
-    CentralComponent
-  ],
+  ] ,
   imports: [
     BrowserModule,
     AppRoutingModule
