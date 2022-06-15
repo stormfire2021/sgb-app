b77249055cb4335886041d1b1e16c9ff0a37747f
Fabiula Pellicciotti <fabiula.pellicciotti@gmail.com>
2022-06-15 10:12:37 -0300

    Update atividade10 - 2

--- src/app/model/User.ts
+++ src/app/model/User.ts
@@ -1,15 +1,17 @@
 export class User {
   id: string;
+  username: string;
   email: string;
-  user: string;
   password: string;
   permission: string;
 
-  constructor( email: string, user: string, password: string, permission: string) {
+  constructor( username: string ,email: string,  password: string, permission: string) {
     this.id = String(Math.round(Math.random() * 1000));
     this.email = email;
-    this.user = user;
+    this.username = username;
     this.password = password;
     this.permission = permission;
   }
+
+
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { User } from '../model/User';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  user!: User;
+  users: User[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  formulario: any;
+  constructor() {
+    this.users = []
   }
 
+  ngOnInit(): void {
+     this.getAll();
+     this.formulario = new FormGroup({
+      id: new FormControl(),
+      username: new FormControl(),
+      email: new FormControl(),
+      password: new FormControl(),
+      permission: new FormControl()
+     })
+
+
+  }
+
+  onSubmit() : void {
+    this.formulario.value.id = String(Math.round(Math.random() * 100));
+    const usuario : User = this.formulario.value;
+    this.users.push(usuario);
+    localStorage.setItem('user', JSON.stringify(this.users))
+    this.formulario.reset();
+  }
+
+  getAll() : void{
+    if(localStorage.getItem('user')){
+      this.users = JSON.parse(localStorage.getItem('user')!);
+    }else{
+      this.users = [];
+    }
+
+  }
 }
